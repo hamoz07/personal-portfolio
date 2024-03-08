9e1ef833c4b305229711eece698d65f840da754d
hamoz07 <hamozaar07@gmail.com>
2024-03-08 15:30:06 +0200

    added new projects and anhanced structure and removed unncessary projects

--- src/projects.js
+++ src/projects.js
@@ -1,29 +1,21 @@
 export const projects = [
   {
-    name: "admin-dashbaord-material-ui",
-    gitrepo: "https://github.com/hamoz07/admin-dashbaord-material-ui.git",
-    technologies: "react",
-    has_pages: "vercel",
-    website:"https://admin-dashbaord-material-ui.vercel.app/",
-    description: "A admin dashboard built using Material UI and React.",
-    path: "../photos/dashboard-ui.png"
-  },
-  {
-    name: "bainwriting-webApp",
+    name: "bainWriting webApp",
     gitrepo: "https://github.com/hamoz07/bainwriting-webApp.git",
     technologies: "js",
     has_pages: "vercel",
     website: "https://bainwriting-web-app.vercel.app/",
-    description: "Bain Writing is a web app that allows users to write, save, and share their writing work with others.",
-    path:"../photos/bainwritingwebapp.png"
+    description:
+      "Bain Writing is a web app that allows users to write, save, and share their writing work with others.",
+    path: "../photos/bainwritingwebapp.png",
   },
   {
-    name: "basic-redux-shopping-cart",
+    name: "basic shopping cart (e-commerce)",
     gitrepo: "https://github.com/hamoz07/basic-redux-shopping-cart.git",
     technologies: "react",
     has_pages: "vercel",
     website: "http://redux-e-commerce.surge.sh/",
-    path:"../photos/redux.png"
+    path: "../photos/redux.png",
   },
   {
     name: "budget-tracker-web-application",
@@ -31,183 +23,158 @@ export const projects = [
     technologies: "react",
     has_pages: "vercel",
     website: "https://budget-tracker-web-application.vercel.app/",
-    path:"../photos/budget-tracker.png"
+    path: "../photos/download (21).png",
   },
   {
-    name: "calc-Liter-Per-Hour",
+    name: "admin-dashbaord-material-ui",
+    gitrepo: "https://github.com/hamoz07/admin-dashbaord-material-ui.git",
+    technologies: "react",
+    has_pages: "vercel",
+    website: "https://admin-dashbaord-material-ui.vercel.app/",
+    description: "A admin dashboard built using Material UI and React.",
+    path: "../photos/dashboard-ui.png",
+  },
+  {
+    name: "Keep Hydrated - calculate Liter Per Hour",
     gitrepo: "https://github.com/hamoz07/calc-Liter-Per-Hour.git",
     technologies: "js",
     has_pages: true,
-    path:"../photos/calc.png"
+    path: "../photos/calc.png",
   },
-
   {
-    name: "car-rental",
+    name: "car rental website",
     gitrepo: "https://github.com/hamoz07/car-rental.git",
     technologies: "css",
     has_pages: true,
-    path:"../photos/car-rental.png"
+    path: "../photos/car-rental.png",
   },
   {
-    name: "clipboard",
+    name: "clipboard manager company landing page",
     gitrepo: "https://github.com/hamoz07/clipboard.git",
     technologies: "css",
     has_pages: true,
-    path:"../photos/clipboard.png"
+    path: "../photos/clipboard.png",
   },
   {
     name: "clock",
     gitrepo: "https://github.com/hamoz07/clock.git",
     technologies: "js",
     has_pages: true,
-    path:"../photos/clock.png"
+    path: "../photos/clock.png",
   },
   {
-    name: "coffee-shop-template",
+    name: "coffee shop website template",
     gitrepo: "https://github.com/hamoz07/coffee-shop-template.git",
     technologies: "css",
-    has_pages:true,
-    path:"../photos/coffeshop.png"
+    has_pages: true,
+    path: "../photos/coffeshop.png",
   },
   {
     name: "count-down",
     gitrepo: "https://github.com/hamoz07/count-down.git",
     technologies: "js",
     has_pages: true,
-    path:"../photos/count-down.png"
+    path: "../photos/count-down.png",
   },
   {
-    name: "CRUD",
+    name: "manage users (CRUD)",
     gitrepo: "https://github.com/hamoz07/CRUD.git",
     technologies: "js",
     has_pages: true,
-    path:"../photos/crud.png"
+    path: "../photos/crud.png",
   },
-
   {
     name: "E-COMMERCE-DASHBOARD-MANAGEMENT-SYSTEM",
     gitrepo:
       "https://github.com/hamoz07/E-COMMERCE-DASHBOARD-MANAGEMENT-SYSTEM.git",
     technologies: "react",
     has_pages: false,
-    path:"../photos/E-COMMERCE-DASHBOARD-MANAGEMENT-SYSTEM.png"
+    path: "../photos/E-COMMERCE-DASHBOARD-MANAGEMENT-SYSTEM.png",
   },
   {
     name: "Customer Relation Management (CRM)",
     gitrepo: "https://github.com/hamoz07/emloyee-system-tracking-status.git",
     technologies: "react",
     has_pages: false,
-    path:"../photos/Customer Relation Management (CRM).png"
+    path: "../photos/Customer Relation Management (CRM).png",
   },
   {
     gitrepo: "https://github.com/hamoz07/firstTemplate.git",
-    name: "firstTemplate",
+    name: "Leon landing page",
     technologies: "css",
     has_pages: true,
-    path:"../photos/template1.png"
+    path: "../photos/template1.png",
   },
   {
-    name: "FOOD",
+    name: "FOOD website template",
     gitrepo: "https://github.com/hamoz07/FOOD.git",
     technologies: "css",
     has_pages: true,
-    path:"../photos/foodshop.png"
+    path: "../photos/foodshop.png",
   },
   {
-    name: "four-cards-challenge",
-    gitrepo: "https://github.com/hamoz07/four-cards-challenge.git",
-    technologies: "css",
-    has_pages: true,
-    path:"../photos/fourcards.png"
-  },
-
-  {
-    name: "huddle_challenge",
+    name: "huddle landing",
     gitrepo: "https://github.com/hamoz07/huddle_challenge.git",
     technologies: "css",
     has_pages: true,
-    path:"../photos/huddlelanding.png"
+    path: "../photos/huddlelanding.png",
   },
   {
-    name: "issue-tracker",
+    name: "issue tracker",
     gitrepo: "https://github.com/hamoz07/issue-tracker.git",
     technologies: "js",
     has_pages: true,
-    path:"../photos/jsissuetracker.png"
+    path: "../photos/jsissuetracker.png",
   },
   {
     name: "JS-PRODUCTS-MANAGEMENT-SYSTEM",
     gitrepo: "https://github.com/hamoz07/JS-PRODUCTS-MANAGEMENT-SYSTEM.git",
     technologies: "js",
     has_pages: true,
-    path:"../photos/crudjs.png"
+    path: "../photos/crudjs.png",
   },
   {
     name: "JS-SLIDESHOW",
     gitrepo: "https://github.com/hamoz07/JS-SLIDESHOW.git",
     technologies: "js",
     has_pages: true,
-    path:"../photos/slideshow.png"
+    path: "../photos/slideshow.png",
   },
   {
     name: "JS-XO-GAME",
     gitrepo: "https://github.com/hamoz07/JS-XO-GAME.git",
     technologies: "js",
     has_pages: true,
-    path:"../photos/xogamejs.png"
+    path: "../photos/xogamejs.png",
   },
   {
     name: "LOCAL-TAPAS",
     gitrepo: "https://github.com/hamoz07/LOCAL-TAPAS.git",
     technologies: "js",
     has_pages: true,
-    path:"../photos/localtapas.png"
-  },
-  {
-    name: "order-summary-card-css-challenge",
-    gitrepo: "https://github.com/hamoz07/order-summary-card-css-challenge.git",
-    technologies: "css",
-    has_pages: true,
-    path:"../photos/order-summarycard.png"
+    path: "../photos/localtapas.png",
   },
   {
     name: "pepsi-landing-page",
     gitrepo: "https://github.com/hamoz07/pepsi-landing-page.git",
     technologies: "css",
     has_pages: true,
-    path:"../photos/pepsilanding.png"
+    path: "../photos/pepsilanding.png",
   },
-
   {
-    name: "product_preview-card",
-    gitrepo: "https://github.com/hamoz07/product_preview-card.git",
-    technologies: "css",
-    has_pages: true,
-    path:"../photos/perfume.png"
-  },
-
-  {
-    name: "qr-code_challenge",
-    gitrepo: "https://github.com/hamoz07/qr-code_challenge.git",
-    technologies: "css",
-    has_pages: true,
-    path:"../photos/qrcode.png"
-  },
-
-  {
-    name: "JS-E-COMMERCE",
+    name: "E-COMMERCE using javascript",
     gitrepo: "https://github.com/hamoz07/JS-E-COMMERCE.git",
     technologies: "js",
     has_pages: true,
-    path:"../photos/Screenshot 2023-12-24 191956.png"
+    path: "../photos/Screenshot 2023-12-24 191956.png",
   },
   {
-    name: "react-advanced-project",
+    name: "To-do list using React",
     gitrepo: "https://github.com/hamoz07/react-advanced-project.git",
     technologies: "react",
     has_pages: "vercel",
     website: "https://advanced-todo-list-manager.vercel.app/",
-    path:"../photos/react-todo.png"
+    path: "../photos/react-todo.png",
   },
   {
     name: "React-Prayers-Timing",
@@ -215,118 +182,115 @@ export const projects = [
     technologies: "react",
     has_pages: "vercel",
     website: "https://react-prayers-timing-u4jj.vercel.app/",
-    path:"../photos/prayer.png"
+    path: "../photos/prayer.png",
   },
   {
     name: "recipes-template",
     gitrepo: "https://github.com/hamoz07/recipes-template.git",
     technologies: "react",
     has_pages: "vercel",
-    website: "http://foodera-template.surge.sh/",
-    path:"../photos/food-recipes.png"
+    website: "https://recipe-template.surge.sh/",
+    path: "../photos/food-recipes.png",
   },
-  {
-    name: "Review-card",
-    gitrepo: "https://github.com/hamoz07/Review-card.git",
-    technologies: "react",
-    has_pages: "vercel",
-    website: "https://review-card-seven.vercel.app/",
-    path:"../photos/card-review.png"
-  },
-
   {
     name: "show-users-posts-challenge",
     gitrepo: "https://github.com/hamoz07/show-users-posts-challenge.git",
     technologies: "js",
     has_pages: true,
-    path:"../photos/show-posts-challenge.png"
+    path: "../photos/show-posts-challenge.png",
   },
-
-
   {
     name: "simple-password-validation",
     gitrepo: "https://github.com/hamoz07/simple-password-validation.git",
     technologies: "js",
     has_pages: true,
-    path:"../photos/password-again.png"
+    path: "../photos/password-again.png",
   },
   {
-    name: "sorting-ecommerce-products-web-application",
+    name: "advanced sorting products e-commerce",
     gitrepo:
       "https://github.com/hamoz07/sorting-ecommerce-products-web-application.git",
     technologies: "react",
     has_pages: "vercel",
     website: "https://sorting-ecommerce-products-web-application.vercel.app/",
-    path:"../photos/sorting-ecommerce.png"
+    path: "../photos/sorting-ecommerce.png",
   },
-
   {
-    name: "tech-ordering-web-app",
+    name: "ordering UBL products",
     gitrepo: "https://github.com/hamoz07/tech-ordering-web-app.git",
     technologies: "react",
     has_pages: "vercel",
     website: "http://tech-online-shop.surge.sh/",
-    path:"../photos/tech-ecommerce.png"
+    path: "../photos/tech-ecommerce.png",
   },
   {
-    name: "template-2",
+    name: "Kasper company introduction website",
     gitrepo: "https://github.com/hamoz07/template-2.git",
     technologies: "css",
     has_pages: true,
-    path:"../photos/template-2.png"
+    path: "../photos/template-2.png",
   },
   {
-    name: "Template-3",
+    name: "An experimental personal introductory and commercial website",
     gitrepo: "https://github.com/hamoz07/Template-3.git",
     technologies: "css",
     has_pages: true,
-    path:"../photos/template-3.png"
+    path: "../photos/template-3.png",
   },
   {
-    name: "template-4",
+    name: "Elzero Control Panel",
     gitrepo: "https://github.com/hamoz07/template-4.git",
     technologies: "css",
     has_pages: true,
-    path:"../photos/template-4.png"
+    path: "../photos/template-4.png",
   },
-
   {
-    name: "translator",
+    name: "translator from-to any language",
     gitrepo: "https://github.com/hamoz07/translator.git",
     technologies: "js",
     has_pages: true,
-    path:"../photos/translatorjs.png"
+    path: "../photos/translatorjs.png",
   },
   {
     name: "type-a-head",
     gitrepo: "https://github.com/hamoz07/type-a-head.git",
     technologies: "js",
     has_pages: true,
-    path:"../photos/type-a-head.png"
-  },
-  {
-    name: "typewriter-effect",
-    gitrepo: "https://github.com/hamoz07/typewriter-effect.git",
-    technologies: "js",
-    has_pages: true,
-    path:"../photos/typewriter.png"
+    path: "../photos/type-a-head.png",
   },
   {
     name: "Typing-speed-game",
     gitrepo: "https://github.com/hamoz07/Typing-speed-game.git",
     technologies: "js",
     has_pages: true,
-    path:"../photos/typing-speed-game.png"
+    path: "../photos/typing-speed-game.png",
   },
   {
     name: "Notes-Web-Applications",
     gitrepo: "https://github.com/hamoz07/Notes-Web-Applications.git",
     technologies: "react+typescript",
     has_pages: "vercel",
-    website:"https://simple-notes-web-applications.vercel.app",
-    path:"../photos/Screenshot 2023-12-24 195059.png"
+    website: "https://simple-notes-web-applications.vercel.app",
+    path: "../photos/Screenshot 2023-12-24 195059.png",
   },
-
+  {
+    name: "products management system using react",
+    gitrepo: "https://github.com/hamoz07/React-ADVANCED-CRUD",
+    technologies: "react+typescript",
+    has_pages: "vercel",
+    website: "https://products-advanced-crud-system.vercel.app/",
+    path: "../photos/screencapture-products-advanced-crud-system-vercel-app-2024-03-08-15_15_16.png",
+    description:
+      "this project where you can add products with details like: (title,description,price,colors of products, product of image, category) , edit them with complete validation form process, delete product, and get back to added products even after refreshing page",
+  },
+  {
+    name: "custom alerts using React + SASS + Typescript",
+    gitrepo: "https://github.com/hamoz07/React-alerts-components",
+    technologies: "react+typescript",
+    has_pages: "vercel",
+    website: "https://react-alerts-components.vercel.app/",
+    path: "../photos/screencapture-react-alerts-components-vercel-app-2024-03-08-15_12_56.png",
+    description:
+      "this project where I applied on SASS and Typescript after I learned them",
+  },
 ];
-
-// "sum generator"
